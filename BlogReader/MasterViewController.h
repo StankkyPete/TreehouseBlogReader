5208033dc8b48f38db01fed76b88910112927feb
StankkyPete <Peter@Peters-MacBook-Air.local>
2015-01-02 16:56:10 -0500

    Initial Commit

---
+++ BlogReader/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  BlogReader
+//
+//  Created by Peter Dorsaneo on 1/2/15.
+//  Copyright (c) 2015 Peter Dorsaneo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
---
+++ BlogReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by Peter Dorsaneo on 1/2/15.
+//  Copyright (c) 2015 Peter Dorsaneo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
